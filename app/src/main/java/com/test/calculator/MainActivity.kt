cec69e9d90f25856670fc33e78e8010d95e7f69f
Pratham Batra <pratham11.batra@gmail.com>
2023-09-14 16:55:56 +0530

    fixed btn7

--- app/src/main/java/com/test/calculator/MainActivity.kt
+++ app/src/main/java/com/test/calculator/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.test.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 //  Op is created to change the output screen
